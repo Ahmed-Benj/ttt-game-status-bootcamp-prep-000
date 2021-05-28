197b70d377e2aef82acda06e583b630d955f46d6
beaming-gamer-888d <ahmedblaugrana@gmail.com>
2021-05-28 23:52:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,12 +6,31 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0,1,2]
-  [3,4,5]
-  [6,7,8]
-  [0,3,6]
-  [1,4,7]
-  [2,5,8]
-  [0,4,8]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
   [2,4,6]
 ]
+
+def won(board)
+
+  for win_combination in WIN_COMBINATIONS
+    win_index_0 = win_combination[0]
+    win_index_1 = win_combination[1]
+    win_index_2 = win_combination[2]
+  end
+  
+  position_0 = board[win_index_0]
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  
+  if position_0=="X" && position_1=="X" && position_2=="X"
+    return win_combination
+  else
+    false
+  end
+end
